ddb4c3a4818661aacc78a6d08b884bde86e29c2b
Jesus Leiva Guerrero <jleiva-g@c2r2s3.42malaga.com>
2025-10-15 19:41:55 +0200

    added scanf

--- scanf/scanf.c
+++ scanf/scanf.c
@@ -6,7 +6,7 @@
 /*   By: jleiva-g <jleiva-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 20:30:55 by jleiva-g          #+#    #+#             */
-/*   Updated: 2025/10/13 20:31:30 by jleiva-g         ###   ########.fr       */
+/*   Updated: 2025/10/15 18:13:03 by jleiva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,123 @@
 
 int match_space(FILE *f)
 {
+	int	c;
+	while ((c = fgetc(f)) != EOF)
+	{
+		if (!isspace(c))
+		{
+			ungetc(c, f);
+			return 0;
+		}
+	}
+	if (ferror(f))
+		return -1;
 	return (0);
 }
 
 int match_char(FILE *f, char c)
 {
+	int	c_read = fgetc(f);
+	if (c_read == EOF)
+	{
+		if (ferror(f))
+			return -1;
+		return 0;
+	}
+	if (c_read == c)
+		return 1;
+	else
+	{
+		ungetc(c_read, f);
+		return 0;
+	}
 	return (0);
 }
 
 int scan_char(FILE *f, va_list ap)
 {
-	return (0);
+	int	c = fgetc(f);
+	if (c == EOF)
+	{
+		if (ferror(f))
+			return -1;
+		return 0;
+	}
+	char	*c_ptr = va_arg(ap, char *);
+	*c_ptr = (char) c;
+	return (1);
 }
 
 int scan_int(FILE *f, va_list ap)
 {
-	return (0);
+	int	c = fgetc(f);
+	if (c == EOF)
+	{
+		if (ferror(f))
+			return -1;
+		return 0;
+	}
+	int	sign = 1;
+	if (c == '+' || c == '-')
+	{
+		if (c == '-')
+			sign *= -1;
+	}
+	else
+		ungetc(c, f);
+	int	value = 0;
+	int	digits = 0;
+	while ((c = fgetc(f)) != EOF)
+	{
+		if (isdigit(c))
+		{
+			value = value * 10 + (c - '0');
+			digits++;
+		}
+		else
+		{
+			ungetc(c, f);
+			break;
+		}
+	}
+	if (ferror(f))
+		return -1;
+	if (digits == 0)
+		return 0;
+	int	*num_ptr = va_arg(ap, int *);
+	*num_ptr = value * sign;
+	return (1);
 }
 
 int scan_string(FILE *f, va_list ap)
 {
-	return (0);
+	int	c = fgetc(f);
+	if (c == EOF)
+	{
+		if (ferror(f))
+			return -1;
+		return 0;
+	}
+	ungetc(c, f);
+	char	*s = va_arg(ap, char *);
+	int	chars = 0;
+	while ((c = fgetc(f)) != EOF)
+	{
+		if (isspace(c))
+		{
+			ungetc(c, f);
+			break;
+		}
+		*s = (char) c;
+		s++;
+		chars++;
+	}
+	if (ferror(f))
+		return -1;
+	if (!chars)
+		return 0;
+	*s = '\0';
+	return (1);
 }
 
 
@@ -96,6 +192,24 @@ int ft_vfscanf(FILE *f, const char *format, va_list ap)
 
 int ft_scanf(const char *format, ...)
 {
+	va_list ap;
+	va_start(ap, format);
 	int ret = ft_vfscanf(stdin, format, ap);
+	va_end(ap);
 	return ret;
+}
+
+int main()
+{
+	int number;
+	char character;
+	char string[500];
+	// ft_scanf("%d", &number);
+	// ft_scanf("%c", &character);
+	// ft_scanf("%s", &string);
+	ft_scanf("%d %c %s", &number, &character, &string);
+	printf("the number you entered is %d\n", number);
+	printf("the character you entered is %c\n", character);
+	printf("the string you entered is %s\n", string);
+	return 0;
 }
