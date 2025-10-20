016ce0bb714246783b8afc5400409ac1469e1154
Jesus Leiva Guerrero <jleiva-g@c2r2s5.42malaga.com>
2025-10-20 18:30:43 +0200

    added fixed gnl

---
+++ gnl/fixed.c
@@ -0,0 +1,98 @@
+#include "fixed.h"
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i=0;
+	while (s[i] && s[i] != c)
+		i++;
+	if (s[i] == c)
+		return s+i;
+	else
+		return NULL;
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	while (n--)
+		((char *) dest)[n] = ((char *) src)[n];
+	return dest;
+}
+
+size_t	ft_strlen (char *s)
+{
+	size_t	ret = 0;
+	while (s && *s)
+	{
+		s++;
+		ret++;
+	}
+	return (ret);
+}
+
+int	str_append_mem(char **s1, char *s2, size_t size2)
+{
+	size_t	size1 = ft_strlen(*s1);
+	char	*tmp = malloc (size2 + size1 + 1);
+	if (!tmp)
+		return 0;
+	ft_memcpy(tmp, *s1, size1);
+	ft_memcpy(tmp + size1, s2, size2);
+	tmp[size1 + size2] = 0;
+	free(*s1);
+	*s1 = tmp;
+	return 1;
+}
+
+int	str_append_str(char **s1, char *s2)
+{
+	return str_append_mem(s1, s2, ft_strlen(s2));
+}
+
+// void *ft_memmove(void *dest, const void *src, size_t n)
+// {
+// 	if (dest > src)
+// 		return ft_memcpy(dest, src, n);
+// 	else if (dest == src || !n)
+// 		return dest;
+// 	size_t	len = ft_strlen((char *)src - 1);
+// 	size_t	i = 0;
+// 	while (i < len)
+// 	{
+// 		((char *)dest)[i] = ((char *)src)[i];
+// 		i++;
+// 	}
+// 	return dest;
+// }
+
+char	*get_next_line (int fd)
+{
+	char	b[BUFFER_SIZE + 1] = "";
+	char	*ret = NULL;
+	char	*tmp = ft_strchr(b, '\n');
+	while (!tmp)
+	{
+		if (!str_append_str(&ret, b))
+			return NULL;
+		int	read_ret = read(fd, b, BUFFER_SIZE);
+		if (read_ret == -1)
+			return NULL;
+		b[read_ret] = 0;
+		tmp = ft_strchr(b, '\n');
+		if (!read_ret)
+			break;
+	}
+	if (!str_append_mem(&ret, b, ft_strlen(b) + 1))
+	{
+		free(ret);
+		return NULL;
+	}
+	return ret;
+}
+
+int	main(void)
+{
+	char *caca;
+	caca = get_next_line(0);
+	write(1, caca, 5);
+	return 0;
+}
---
+++ gnl/fixed.h
@@ -0,0 +1,9 @@
+#ifndef GNL
+# define GNL
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+# include <unistd.h>
+# include <stdlib.h>
+char *get_next_line(int fd);
+#endif
